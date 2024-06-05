518d5a9c2f8eb84c3c66dfb18c67d9ae39c1aedd
Le Van Hoang Phi <phipk02@gmail.com>
2024-06-05 11:12:11 +0700

    [update] just print 2 loss

--- train.py
+++ train.py
@@ -213,8 +213,6 @@ if __name__ == "__main__":
         writer.add_scalar("loss/dis/real_cls", real_loss_cls, it)
         writer.add_scalar("loss/dis/real_adv", real_loss_adv, it)
 
-        print(
-            f"Done iteration =[{it}]=, gen_loss: {gen_loss}, dis_loss: {dis_loss}, loss_c: {loss_c}, loss_s: {loss_s}, loss_adv: {loss_adv}, loss_cls: {loss_cls}"
-        )
+        print(f"Done iteration =[{it}]=, loss_c: {loss_c}, loss_s: {loss_s}")
 
 writer.close()
