f6fb2e45daae79f3fa0d42c658f151d5bec7aa4a
Le Van Hoang Phi <phipk02@gmail.com>
2024-06-29 18:32:39 +0700

    [add] inference

--- test.py
+++ test.py
@@ -5,37 +5,88 @@ from models import create_model
 from util.visualizer import save_images
 from util import html
 import util.util as util
+from PIL import Image
+from data.base_dataset import get_transform
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     opt = TestOptions().parse()  # get test options
     # hard-code some parameters for test
-    opt.num_threads = 0   # test code only supports num_threads = 1
-    opt.batch_size = 1    # test code only supports batch_size = 1
+    opt.num_threads = 0  # test code only supports num_threads = 1
+    opt.batch_size = 1  # test code only supports batch_size = 1
     opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
-    opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
-    opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
-    dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
+    opt.no_flip = (
+        True  # no flip; comment this line if results on flipped images are needed.
+    )
+    opt.display_id = (
+        -1
+    )  # no visdom display; the test code saves the results to a HTML file.
+    dataset = create_dataset(
+        opt
+    )  # create a dataset given opt.dataset_mode and other options
     # train_dataset = create_dataset(util.copyconf(opt, phase="train"))
-    model = create_model(opt)      # create a model given opt.model and other options
+    model = create_model(opt)  # create a model given opt.model and other options
     # create a webpage for viewing the results
-    web_dir = os.path.join(opt.results_dir, opt.name, '{}_{}'.format(opt.phase, opt.epoch))  # define the website directory
-    print('creating web directory', web_dir)
-    webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.epoch))
+    web_dir = os.path.join(
+        opt.results_dir, opt.name, "{}_{}".format(opt.phase, opt.epoch)
+    )  # define the website directory
+    print("creating web directory", web_dir)
+    webpage = html.HTML(
+        web_dir,
+        "Experiment = %s, Phase = %s, Epoch = %s" % (opt.name, opt.phase, opt.epoch),
+    )
 
-    for i, data in enumerate(dataset):
-        if i == 0:
-            model.setup(opt)               # regular setup: load and print networks; create schedulers
-            model.parallelize()
-            if opt.eval:
-                model.eval()
-        if i >= opt.num_test:  # only apply our model to opt.num_test images.
-            break
-        model.set_input(data)  # unpack data from data loader
-        model.test()           # run inference
-        visuals = model.get_current_visuals()  # get image results
-        img_path = model.get_image_paths()     # get image paths
-        if i % 5 == 0:  # save images to an HTML file
-            print('processing (%04d)-th image... %s' % (i, img_path))
-        save_images(webpage, visuals, img_path, width=opt.display_winsize)
+    modified_opt = util.copyconf(
+        opt,
+        load_size=opt.load_size,
+    )
+    transform = get_transform(modified_opt)
+
+    A_paths = os.listdir("./dataset/inference/valA")
+    B_paths = os.listdir("./dataset/inference/valB/test")
+
+    model.setup(opt)  # regular setup: load and print networks; schedulers
+    model.parallelize()
+    i = 1
+    for A_path in A_paths:
+        A_path = os.path.join("./dataset/inference/valA", A_path)
+        A_img = Image.open(A_path).convert("RGB")
+        A = transform(A_img)
+        A = A.unsqueeze(0)
+
+        for B_path in B_paths:
+            # try:
+            B_path = os.path.join("./dataset/inference/valB/test", B_path)
+
+            B_img = Image.open(B_path).convert("RGB")
+            B = transform(B_img)
+            # add batch size dimension to A and B
+            B = B.unsqueeze(0)
+            # print("A shape", A.shape, "B shape", B.shape)
+
+            model.set_input({"A": A, "B": B, "A_paths": A_path, "B_paths": B_path})
+            model.test()  # run inference
+            visuals = model.get_current_visuals()  # get image results
+            img_path = model.get_image_paths()  # get image paths
+            # if i % 5 == 0:  # save images to an HTML file
+            print("processing (%04d)-th image... %s" % (i, img_path))
+            save_images(webpage, visuals, img_path, width=opt.display_winsize, i=i)
+            i += 1
+
     webpage.save()  # save the HTML
+
+    # for i, data in enumerate(dataset):
+    #     if i == 0:
+    #         model.setup(opt)  # regular setup: load and print networks; schedulers
+    #         model.parallelize()
+    #         if opt.eval:
+    #             model.eval()
+    #     if i >= opt.num_test:  # only apply our model to opt.num_test images.
+    #         break
+    #     model.set_input(data)  # unpack data from data loader
+    #     model.test()  # run inference
+    #     visuals = model.get_current_visuals()  # get image results
+    #     img_path = model.get_image_paths()  # get image paths
+    #     # if i % 5 == 0:  # save images to an HTML file
+    #     print("processing (%04d)-th image... %s" % (i, img_path))
+    #     save_images(webpage, visuals, img_path, width=opt.display_winsize)
+    # webpage.save()  # save the HTML
--- train.py
+++ train.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
     print("The number of training images = %d" % dataset_size)
 
     model = create_model(opt)  # create a model given opt.model and other options
-
     visualizer = Visualizer(
         opt
     )  # create a visualizer that display/save images and plots
@@ -35,64 +34,73 @@ if __name__ == "__main__":
 
         dataset.set_epoch(epoch)
         for i, data in enumerate(dataset):  # inner loop within one epoch
-            iter_start_time = time.time()  # timer for computation per iteration
-            if total_iters % opt.print_freq == 0:
-                t_data = iter_start_time - iter_data_time
+            try:
+                iter_start_time = time.time()  # timer for computation per iteration
+                if total_iters % opt.print_freq == 0:
+                    t_data = iter_start_time - iter_data_time
 
-            batch_size = data["A"].size(0)
-            total_iters += batch_size
-            epoch_iter += batch_size
-            if len(opt.gpu_ids) > 0:
-                torch.cuda.synchronize()
-            optimize_start_time = time.time()
-            if epoch == opt.epoch_count and i == 0:
-                model.setup(
-                    opt
-                )  # regular setup: load and print networks; create schedulers
-                model.parallelize()
-            model.set_input(data)  # unpack data from dataset and apply preprocessing
-            model.optimize_parameters()  # calculate loss functions, get gradients, update network weights
-            if len(opt.gpu_ids) > 0:
-                torch.cuda.synchronize()
-            optimize_time = (
-                time.time() - optimize_start_time
-            ) / batch_size * 0.005 + 0.995 * optimize_time
+                batch_size = data["A"].size(0)
+                total_iters += batch_size
+                epoch_iter += batch_size
+                if len(opt.gpu_ids) > 0:
+                    torch.cuda.synchronize()
+                optimize_start_time = time.time()
+                if epoch == opt.epoch_count and i == 0:
+                    model.setup(
+                        opt
+                    )  # regular setup: load and print networks; create schedulers
+                    model.parallelize()
+                model.set_input(
+                    data
+                )  # unpack data from dataset and apply preprocessing
 
-            if (
-                total_iters % opt.display_freq == 0
-            ):  # display images on visdom and save images to a HTML file
-                save_result = total_iters % opt.update_html_freq == 0
-                model.compute_visuals()
-                visualizer.display_current_results(
-                    model.get_current_visuals(), epoch, save_result
-                )
+                model.optimize_parameters()  # calculate loss functions, get gradients, update network weights
+                if len(opt.gpu_ids) > 0:
+                    torch.cuda.synchronize()
+                optimize_time = (
+                    time.time() - optimize_start_time
+                ) / batch_size * 0.005 + 0.995 * optimize_time
 
-            if (
-                total_iters % opt.print_freq == 0
-            ):  # print training losses and save logging information to the disk
-                losses = model.get_current_losses()
-                visualizer.print_current_losses(
-                    epoch, epoch_iter, losses, optimize_time, t_data
-                )
-                if opt.display_id is None or opt.display_id > 0:
-                    visualizer.plot_current_losses(
-                        epoch, float(epoch_iter) / dataset_size, losses
+                if (
+                    total_iters % opt.display_freq == 0
+                ):  # display images on visdom and save images to a HTML file
+                    save_result = total_iters % opt.update_html_freq == 0
+                    model.compute_visuals()
+                    visualizer.display_current_results(
+                        model.get_current_visuals(), epoch, save_result
                     )
 
-            if (
-                total_iters % opt.save_latest_freq == 0
-            ):  # cache our latest model every <save_latest_freq> iterations
-                print(
-                    "saving the latest model (epoch %d, total_iters %d)"
-                    % (epoch, total_iters)
-                )
-                print(
-                    opt.name
-                )  # it's useful to occasionally show the experiment name on console
-                save_suffix = "iter_%d" % total_iters if opt.save_by_iter else "latest"
-                model.save_networks(save_suffix)
+                if (
+                    total_iters % opt.print_freq == 0
+                ):  # print training losses and save logging information to the disk
+                    losses = model.get_current_losses()
+                    visualizer.print_current_losses(
+                        epoch, epoch_iter, losses, optimize_time, t_data
+                    )
+                    if opt.display_id is None or opt.display_id > 0:
+                        visualizer.plot_current_losses(
+                            epoch, float(epoch_iter) / dataset_size, losses
+                        )
 
-            iter_data_time = time.time()
+                if (
+                    total_iters % opt.save_latest_freq == 0
+                ):  # cache our latest model every <save_latest_freq> iterations
+                    print(
+                        "saving the latest model (epoch %d, total_iters %d)"
+                        % (epoch, total_iters)
+                    )
+                    print(
+                        opt.name
+                    )  # it's useful to occasionally show the experiment name on console
+                    save_suffix = (
+                        "iter_%d" % total_iters if opt.save_by_iter else "latest"
+                    )
+                    model.save_networks(save_suffix)
+
+                iter_data_time = time.time()
+            except Exception as e:
+                print(e)
+                continue
 
         if (
             epoch % opt.save_epoch_freq == 0
